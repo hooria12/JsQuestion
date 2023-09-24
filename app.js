3fec4655ae7e2206942ed6c337947df1d20193ba
hooria12 <kashifhooria15@gmail.com>
2023-09-24 09:22:25 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,525 @@
+//    *****************ANSWER NO 1*******************
+// alert("enter name");
+// alert("enter last name");
+// alert("enter email");
+// alert("enter phone number");
+// alert("enter password");
+
+
+// ****************ANSWER NO 2**************************
+// alert("You're learning JavaScript!")
+
+// ****************ANSWER NO 3**************************
+// alert("Always Be happy");
+
+// ************CHAPTER NO 2****************
+// ********************************************************
+
+// ***************ANSWER NO 1***************************
+// var a = "YouTube";
+// document.write(a);
+
+// **************  nswer no 2**************************
+// var a;
+// a = prompt("enter a variable")
+
+
+// var BestMan = "Charlie"
+// console.log(BestMan)
+
+
+// var caseQqty;
+// caseQty = 14;
+
+
+// var num;
+// num = "9";
+// prompt(num);
+
+
+
+// var sum;
+// sum = 1+3;
+
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+
+
+// var a =10;
+// var b = a +3;
+// console.log(b);
+
+
+
+
+
+
+// var poductcost = "3.45";
+
+// var NameOfBand;
+
+// var totalnumber = 150;
+
+
+
+
+
+
+// var HooriaKashif;
+
+// example of question 1;
+// var sum = 5%2;
+// console.log(sum)
+
+// var num = 20 % 6;
+// console.log(num);
+
+// var largeNum = 1000 * 2;
+
+// var a = 10;
+// var b= 5;
+// var subtract = a-b;
+// console.log(subtract);
+
+// var divide;
+// var a = 11;
+// var b = 5;
+// var remainder = 11/5;
+// console.log(remainder)
+
+// var multiply;
+// var a = 5;
+// var b = 7;
+// var result = a*b;
+// alert(result);
+
+
+
+
+
+// var x= x +1;
+// var a = a-5;
+// var b = b/4;
+
+// var x;
+// x = 100;
+// x--;
+// console.log(x);
+
+// var x = 50;
+// var y = x++;
+// console.log(y);
+
+// var y = 50;
+// var z = --y;
+// console.log(z);
+
+
+//  var newNum = 12;
+//   newNum--;
+//  console.log(newNum)
+
+// var a = 10;
+// b= a+1;
+// console.log(b)
+
+// var sum;
+// sum = 5;
+// sum++;
+// alert(sum)
+
+
+
+
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum)
+
+//  var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum)
+
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum)
+
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum)
+
+// var unit =  2 + 2 * 4 + 10;
+// console.log(unit)
+
+// var cost =  4 / 2 * 4;
+// console.log(cost)
+
+
+
+
+// var num = "2" + "2";
+// console.log(num);
+
+// var message = ("Hello," + "Dolly");
+// console.log(message)
+
+// var num;
+//  num = alert("33" + 3);
+
+
+// ********************************************************************
+// *****************************************************************
+// *************************************************************
+// chapter no 8 last solve 
+
+
+// var country;
+// alert("Pakistan Zindabad");
+
+
+// var a = 12;
+// console.log(a +" "+ "14")
+
+// var a = 15;
+// var b = 20;
+// var sum = a+b ;
+// console.log("the sum of two numbers is" + " " + sum);
+
+
+// ****************************************************************
+// ********************************************************
+
+
+
+
+// var a ;
+// prompt("Enter Youe FirstName")
+
+// prompt(" China \n emter your coutry")
+
+// var yourName = prompt("Enter Your Name");
+
+// prompt("null")
+
+// prompt("allah bless you");
+
+// var message =" hello";
+// var num ="How are you?"
+// prompt(message + num);
+
+// var message = prompt("very nice")
+// alert(message);
+
+
+
+
+
+// var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// }
+
+
+// var a = prompt("enter a number");
+// var c = prompt("enter a sign");
+// var b = prompt("enter b number");
+
+// var d = alert(a + b + c);
+
+// var x ="20";
+// var y = "20";
+// if (x === y) {
+//      alert("what is the value of z")
+
+// }
+
+// var  ZipCode = prompt("Enter a Zipcode");
+// if (ZipCode == "10010") {
+//     alert("Karachi")
+// } else {
+//     alert("Please write correct city")
+// }
+
+// var num = prompt("enter a number");
+// if (num !== 100) {
+//     alert("wrong choice");
+// }
+
+
+// var a = 10;
+// var b = 15;
+// if (a>=b) {
+//     alert("good")
+// }
+
+// var x = 10;
+// if (x != 15) {
+//     prompt("enter a number")
+// }
+
+// var a = prompt("Enter a number");
+// var b = prompt("Enter second number");
+// if (a != b) {
+//     alert("congratulations")
+// }
+
+// var Name = prompt("enter a name");
+// if (Name != "ali" ) {
+//     alert("No Match")
+// }
+
+// var a = prompt("enter a number");
+// var b = prompt("enter second number");
+// if (a >= b) {
+//     alert("pass")
+// } else {
+//     alert("fail")
+// }
+
+
+// var obt = prompt("Enter obtained marks");
+// var total = prompt("Enter total marks");
+// var end = obt/total*100;
+// alert(end + " " + "%")
+// if (end >= "80%" ) {
+//     alert("A Grade")
+// } 
+// else {
+//     alert("C Grade")
+// }
+
+// var a = prompt("enter a number")
+// if (a === 10) {
+//     alert("a is 10");
+//    }
+// else{
+//     alert("a is greater")
+// }
+
+// var city = prompt("Enter a city");
+// if (city ==" karachi") {
+//     alert("acknowledging it is Karachi")
+// }
+// else if(city == "lahore");{
+//     alert("acknowledging it is lahore")
+// }
+
+// if (weight > 300 && time < 6) {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+
+// var a = 12;
+// var b = 13;
+// var c = 14;
+// var d = 15;
+// if(a==b &&c==d);
+// {
+//     alert("write")
+// }
+
+// var a = prompt("enter a num");
+// var b = prompt("enter b num");
+// if (a == b || a <= b) {
+//     alert("good")
+// } else {
+//     alert("bad")
+// }
+
+var a = "ali";
+var b = "ahmed";
+var c = prompt("enter first name");
+var d = prompt("enter second name");
+if (a == c && b == d) {
+    alert("thanks")
+} else {
+    alert("correct it")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
